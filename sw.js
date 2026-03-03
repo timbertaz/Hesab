40e4205a844ffbe7a4ae01cbf6135ca3c6b16efb
timbertaz <timbertaz1@gmail.com>
2026-03-03 20:27:37 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'hesab-v1';
+const FILES = [
+  './hesab.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        return caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      }).catch(function() {
+        return cached;
+      });
+    })
+  );
+});
